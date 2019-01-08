6c8d3d5a531e6f363525e58375af2399b3746545
zkty98 <zkty98@163.com>
2019-01-07 19:43:32 -0500

    add linkedin

---
+++ zkt.py
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Goodbye World</h1>
+<h2>Adios</h2>
+
+<p> What a short website </p>
+
+<a href="goodbye.html"> Goodbye Page </a>
+
+<a href="index.html"> Home Page </a>
+
+<a href="https://www.linkedin.com/in/keting-zhou-40433a118/">  Find me on LinkedIn!</a>
+
+</body>
+</html>
